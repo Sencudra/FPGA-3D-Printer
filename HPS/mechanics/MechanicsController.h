9084251e7d2ccbac04a97fe0f7cdd156e86f4129
Spieler <volodya12309@gmail.com>
2019-04-14 20:25:37 +0300

    add header of endstop_states. Change header of move and move_extruder.

--- HPS/mechanics/MechanicsController.h
+++ HPS/mechanics/MechanicsController.h
@@ -15,8 +15,10 @@ public:
 
     MechanicsController();
 
-    void move(int32_t da, int32_t db, int32_t dz, uint32_t sa, u_int32_t sb, uint32_t sz);
-    void move_extrude(int32_t da, int32_t db, int32_t dz, int32_t de, uint32_t sa, u_int32_t sb, uint32_t sz, u_int32_t se);
+    void move(int32_t da, int32_t db, int32_t dz, uint32_t sa, u_int32_t sb, uint32_t sz,
+               int32_t& ra, int32_t& rb, int32_t& rz);
+    void move_extrude(int32_t da, int32_t db, int32_t dz, int32_t de, uint32_t sa, uint32_t sb,
+                        uint32_t sz, uint32_t se, int32_t& ra, int32_t& rb, int32_t& rz, int32_t& re);
     void auto_home(bool x, bool y, bool z);
     void enable_steppers();
     void disable_steppers();
@@ -24,6 +26,9 @@ public:
     void wait_hotend_temperature(int32_t t);
     void set_bed_temperature(int32_t t);
     void wait_bed_temperature(int32_t t);
+    void endstop_states(bool& xmin, bool& xmax,
+                        bool& ymin, bool& ymax,
+                        bool& zmin, bool& zmax);
 
 };
 
