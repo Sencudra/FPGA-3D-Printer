d8fd9435e31250834237d36b6fe14a200bca0409
Spieler <volodya12309@gmail.com>
2019-04-22 22:41:30 +0300

    add constants, variables. Add get/set methods. Complete methods: move, move_extrude, auto_home, enable/disable_steppers, set/wait_hotend/bed_temperature. Fix bugs in next release.

--- HPS/mechanics/commands.cpp
+++ HPS/mechanics/commands.cpp
@@ -1,134 +1,172 @@
 #include "MechanicsController.h"
 
-void MechanicsController::move(int32_t da, int32_t db, int32_t dz, uint32_t sa, u_int32_t sb, uint32_t sz, int32_t &ra,
-                               int32_t &rb, int32_t &rz) {
-    /*addr->set_stepper_1_speed(speed1);
-    addr->set_stepper_2_speed(speed2);
-    addr->set_stepper_3_speed(speed3);
-    addr->set_stepper_4_speed(0);
-    addr->set_stepper_1_steps_in(steps_count1);
-    addr->set_stepper_2_steps_in(steps_count2);
-    addr->set_stepper_3_steps_in(steps_count3);
-    addr->set_stepper_4_steps_in(0);
+int32_t MechanicsController::voltage_adc(int32_t temp)
+{
+    if (temp >= 0 and temp <= MAX_TEMP)
+    {
+        int i = -1;
+        while (temptable_11[++i][1] > temp);
+        if (temp != temptable_11[i][1])
+        {
+            float k = (float)(temp - temptable_11[i][1])/(temptable_11[i-1][1] - temptable_11[i][1]);
+            return (round((int)temptable_11[i][0] + (int)(temptable_11[i-1][0] - temptable_11[i][0])*k));
+        }
+        else
+            return temptable_11[i][0];
+    }
+    return 3255;
+}
 
-    addr->set_flags_in_start_driving_state(true);
-    while (addr->get_flags_out_stepper_state());
-    addr->set_flags_in_start_driving_state(false);
+int32_t MechanicsController::temperature_adc(int32_t volt)
+{
+    if (volt >= temptable_11[0][0] and volt <= 3255*oversampling_rate)
+    {
+        int i = -1;
+        while (temptable_11[++i][0] < volt);
+        if (volt != temptable_11[i][0])
+        {
+            float k = (float)((int)(volt - temptable_11[i][0]))/(int)((temptable_11[i-1][0] - temptable_11[i][0]));
+            return (round(temptable_11[i][1] + (temptable_11[i-1][1] - temptable_11[i][1])*k));
+        }
+        else return temptable_11[i][1];
+    }
+    return 0;
+}
+
+void MechanicsController::move(int32_t da, int32_t db, int32_t dz, uint32_t sa, uint32_t sb, uint32_t sz, int32_t &ra,
+                               int32_t &rb, int32_t &rz) {
+    *addr_stepper_1_speed = sa;
+    *addr_stepper_2_speed = sb;
+    *addr_stepper_3_speed = sz;
+    *addr_stepper_4_speed = 0;
+
+    *addr_stepper_1_steps_in = da;
+    *addr_stepper_2_steps_in = db;
+    *addr_stepper_3_steps_in = dz;
+    *addr_stepper_4_steps_in = 0;
+
+
+    set_flags_in_start_driving_state(true);
+    while (get_flags_out_stepper_state());
+    set_flags_in_start_driving_state(false);
 
     usleep(2);
 
-    ra = addr->get_stepper_1_steps_out();
-    rb = addr->get_stepper_2_steps_out();
-    rz = addr->get_stepper_3_steps_out();
+    ra = *addr_stepper_1_steps_out;
+    rb = *addr_stepper_2_steps_out;
+    rz = *addr_stepper_3_steps_out;
 
-    addr->set_stepper_1_speed(0);
-    addr->set_stepper_2_speed(0);
-    addr->set_stepper_3_speed(0);
+    *addr_stepper_1_speed = 0;
+    *addr_stepper_2_speed = 0;
+    *addr_stepper_3_speed = 0;
+    *addr_stepper_4_speed = 0;
 
-    addr->set_stepper_1_steps_in(0);
-    addr->set_stepper_2_steps_in(0);
-    addr->set_stepper_3_steps_in(0);*/
+    *addr_stepper_1_steps_in = 0;
+    *addr_stepper_2_steps_in = 0;
+    *addr_stepper_3_steps_in = 0;
+    *addr_stepper_4_steps_in = 0;
 }
 
 void MechanicsController::move_extrude(int32_t da, int32_t db, int32_t dz, int32_t de, uint32_t sa, uint32_t sb,
                                        uint32_t sz, uint32_t se, int32_t &ra, int32_t &rb, int32_t &rz, int32_t &re)
 {
 
-    /*addr->set_stepper_1_speed(speed1);
-    addr->set_stepper_2_speed(speed2);
-    addr->set_stepper_3_speed(speed3);
-    addr->set_stepper_4_speed(speed4);
-    addr->set_stepper_1_steps_in(steps_count1);
-    addr->set_stepper_2_steps_in(steps_count2);
-    addr->set_stepper_3_steps_in(steps_count3);
-    addr->set_stepper_4_steps_in(steps_count4);
+    *addr_stepper_1_speed = sa;
+    *addr_stepper_2_speed = sb;
+    *addr_stepper_3_speed = sz;
+    *addr_stepper_4_speed = se;
 
-    addr->set_flags_in_start_driving_state(true);
-    while (addr->get_flags_out_stepper_state());
+    *addr_stepper_1_steps_in = da;
+    *addr_stepper_2_steps_in = db;
+    *addr_stepper_3_steps_in = dz;
+    *addr_stepper_4_steps_in = de;
 
-    addr->set_flags_in_start_driving_state(false);
+    set_flags_in_start_driving_state(true);
+    while (get_flags_out_stepper_state());
+
+    set_flags_in_start_driving_state(false);
 
     usleep(2);
 
-    ra = addr->get_stepper_1_steps_out();
-    rb = addr->get_stepper_2_steps_out();
-    rz = addr->get_stepper_3_steps_out();
-    re = addr->get_stepper_4_steps_out();
+    ra = *addr_stepper_1_steps_out;
+    rb = *addr_stepper_2_steps_out;
+    rz = *addr_stepper_3_steps_out;
+    re = *addr_stepper_4_steps_out;
 
-    addr->set_stepper_1_speed(0);
-    addr->set_stepper_2_speed(0);
-    addr->set_stepper_3_speed(0);
-    addr->set_stepper_4_speed(0);
+    *addr_stepper_1_speed = 0;
+    *addr_stepper_2_speed = 0;
+    *addr_stepper_3_speed = 0;
+    *addr_stepper_4_speed = 0;
 
-    addr->set_stepper_1_steps_in(0);
-    addr->set_stepper_2_steps_in(0);
-    addr->set_stepper_3_steps_in(0);
-    addr->set_stepper_4_steps_in(0);*/
+    *addr_stepper_1_steps_in = 0;
+    *addr_stepper_2_steps_in = 0;
+    *addr_stepper_3_steps_in = 0;
+    *addr_stepper_4_steps_in = 0;
 }
 
 void MechanicsController::auto_home(bool x, bool y, bool z) {
-  /*  addr->set_flags_in_homex(x);
-    addr->set_flags_in_homey(y);
-    addr->set_flags_in_homez(z);
+    set_flags_in_homex(x);
+    set_flags_in_homey(y);
+    set_flags_in_homez(z);
 
     //надо подправить, т.к. нет скорости
-    addr->set_stepper_1_speed(((min(maxspeed, pos->get_pos_speed()))*stepsperrot*microsteps/rotlength)/60);
-    addr->set_stepper_2_speed(((min(maxspeed, pos->get_pos_speed()))*stepsperrot*microsteps/rotlength)/60);
-    addr->set_stepper_3_speed(((min(maxspeed, pos->get_pos_speed()))*stepsperrot*microsteps/rotlength)/60);
+    *addr_stepper_1_speed = (((fmin(maxspeed, HOMING_FEEDRATE_XY))*stepsperrot*microsteps/rotlength)/60);
+    *addr_stepper_2_speed = (((fmin(maxspeed, HOMING_FEEDRATE_XY))*stepsperrot*microsteps/rotlength)/60);
+    *addr_stepper_3_speed = (((fmin(maxspeed, HOMING_FEEDRATE_Z))*stepsperrot*microsteps/rotlength)/60);
 
-    addr->set_flags_in_start_homing_state(true);
-
-    usleep(2);
-
-    while (addr->get_flags_out_stepper_state());
-
-    addr->set_flags_in_start_homing_state(false);
+    set_flags_in_start_homing_state(true);
 
     usleep(2);
 
-    addr->set_stepper_1_speed(0);
-    addr->set_stepper_2_speed(0);
-    addr->set_stepper_3_speed(0);*/
+    while (get_flags_out_stepper_state());
+
+    set_flags_in_start_homing_state(false);
+
+    usleep(2);
+
+    *addr_stepper_1_speed = 0;
+    *addr_stepper_2_speed = 0;
+    *addr_stepper_3_speed = 0;
 }
 
 void MechanicsController::enable_steppers() {
-//    addr->set_flags_in_stepper_enabled(true);
+    set_flags_in_stepper_enabled(true);
 }
 
 void MechanicsController::disable_steppers() {
-//    addr->set_flags_in_stepper_enabled(false);
+    set_flags_in_stepper_enabled(false);
 }
 
 void MechanicsController::set_hotend_temperature(int32_t t) {
-    /*addr->set_flags_in_heat_extruder_hold(false);
-    addr->set_flags_in_heat_extruder(false);
-    addr->set_temp_e1_upper(voltage_adc(temp));
-    addr->set_flags_in_heat_extruder(true);*/
+    set_flags_in_heat_extruder_hold(false);
+    set_flags_in_heat_extruder(false);
+    set_temp_e1_upper(voltage_adc(t));
+    set_flags_in_heat_extruder(true);
 }
 
 void MechanicsController::wait_hotend_temperature(int32_t t) {
-    /*addr->set_flags_in_heat_extruder(false);
-    addr->set_flags_in_heat_extruder_hold(false);
-    addr->set_temp_e1_bottom(voltage_adc(temp - TEMP_DELTA));
-    addr->set_temp_e1_upper(voltage_adc(temp + TEMP_DELTA));
-    addr->set_flags_in_heat_extruder_hold(true);
+    set_flags_in_heat_extruder(false);
+    set_flags_in_heat_extruder_hold(false);
+    set_temp_e1_bottom(voltage_adc(t - TEMP_DELTA));
+    set_temp_e1_upper(voltage_adc(t + TEMP_DELTA));
+    set_flags_in_heat_extruder_hold(true);
     usleep(2);
-    while (addr->get_flags_out_heating_extruder());*/
+    while (get_flags_out_heating_extruder());
 }
 
 void MechanicsController::set_bed_temperature(int32_t t) {
-    /*addr->set_flags_in_heat_bed_hold(false);
-    addr->set_flags_in_heat_bed(false);
-    addr->set_temp_bed_upper(voltage_adc(temp));
-    addr->set_flags_in_heat_bed(true);*/
+    set_flags_in_heat_bed_hold(false);
+    set_flags_in_heat_bed(false);
+    set_temp_bed_upper(voltage_adc(t));
+    set_flags_in_heat_bed(true);
 }
 
 void MechanicsController::wait_bed_temperature(int32_t t) {
-    /*addr->set_flags_in_heat_bed(false);
-    addr->set_flags_in_heat_bed_hold(false);
-    addr->set_temp_bed_bottom(voltage_adc(temp - TEMP_DELTA));
-    addr->set_temp_bed_upper(voltage_adc(temp + TEMP_DELTA));
-    addr->set_flags_in_heat_bed_hold(true);
+    set_flags_in_heat_bed(false);
+    set_flags_in_heat_bed_hold(false);
+    set_temp_bed_bottom(voltage_adc(t - TEMP_DELTA));
+    set_temp_bed_upper(voltage_adc(t + TEMP_DELTA));
+    set_flags_in_heat_bed_hold(true);
     usleep(2);
-    while (addr->get_flags_out_heating_bed());*/
+    while (get_flags_out_heating_bed());
 }
