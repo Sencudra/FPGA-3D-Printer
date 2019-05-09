a5967ccee29c971d54d1a05beca24aeb607e78ab
Spieler <volodya12309@gmail.com>
2019-05-09 23:30:21 +0300

    delete file.txt

--- HPS/main.cpp
+++ HPS/main.cpp
@@ -5,8 +5,6 @@ using namespace std;
 int main() {
 
   	PrinterController printer;
-  	printer.state = Printing;
-    printer.to_print = "/home/root/file.txt";
   	printer.main_loop();
 
     return 0;
