90ed16cfcfee6988dbd532162323001549ea0abb
Vlad Tarasevich <vlad.tarasevich27@gmail.com>
2019-05-14 16:04:39 +0300

    Revert "Fix Nextion bug (#17)"
    This reverts commit 61e7aea11d87790589a6ec9a04f761acaba85ee8.

--- HPS/config.h
+++
@@ -1,6 +0,0 @@
-#ifndef INC_3D_PRINTER_CONFIG_H
-#define INC_3D_PRINTER_SCREENCONTROLLER_H
-
-extern bool isScreenDebug;
-
-#endif
--- HPS/printer/stl2gcode/Contour.cpp
+++
@@ -1,8 +0,0 @@
-#include "Contour.h"
-
-ostream& operator<<(ostream& stream, const Contour& contour) {
-    for (auto& vertex : contour) {
-        stream << vertex << " ";
-    }
-    return stream;
-}
