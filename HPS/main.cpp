1a428e94393ef0968ef6ed3f1cc1cdc2b0d42fec
Spieler <vvzunin@edu.hse.ru>
2019-05-14 09:43:39 +0300

    fix bugs

--- HPS/main.cpp
+++ HPS/main.cpp
@@ -5,10 +5,6 @@ using namespace std;
 int main() {
 
   	PrinterController printer;
-  	printer.state = Printing;
-  	printer.to_print = "file.txt";
   	printer.main_loop();
-    //TODO: quartus включение только !=0
-    //TODO: quartus signal=1 только когда нужно начинать двигаться
     return 0;
 }
