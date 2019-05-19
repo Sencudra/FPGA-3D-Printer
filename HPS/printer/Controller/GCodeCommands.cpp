65d78337f1278f08762cde55f47bd98726e19783
Vladimir <Volodya12309@gmail.com>
2019-05-19 22:34:59 +0300

    Merge branch "printer" into "develop" (#31)
    * fix bug with coordinate system

--- HPS/printer/Controller/GCodeCommands.cpp
+++ HPS/printer/Controller/GCodeCommands.cpp
@@ -8,18 +8,22 @@ void PrinterController::calc_steps_speed(float dx, float dy, float dz, float de,
     float db = dx - dy; //расстояние, которое должен обработать двигатель b системы core xy
 
     //необходимое для движения количество микрошагов = число оборотов * количество микрошагов за оборот
-    steps_a = int32_t(da * DEFAULT_AXIS_STEPS_PER_UNIT[0]);
-    steps_b = int32_t(db * DEFAULT_AXIS_STEPS_PER_UNIT[1]);
-    steps_e = int32_t(de * DEFAULT_AXIS_STEPS_PER_UNIT[3]);
-    steps_z = int32_t(dz * DEFAULT_AXIS_STEPS_PER_UNIT[2]);
+    steps_a = (int32_t) round(da * DEFAULT_AXIS_STEPS_PER_UNIT[0]);
+    steps_b = (int32_t) round(db * DEFAULT_AXIS_STEPS_PER_UNIT[1]);
+    steps_e = (int32_t) round(de * DEFAULT_AXIS_STEPS_PER_UNIT[3]);
+    steps_z = (int32_t) round(dz * DEFAULT_AXIS_STEPS_PER_UNIT[2]);
 
     //подсчет макс расстояния в микрошагах для опреодоления общего времени
     //позволяет настроить скорость и время для единовременного завершения работы двигателей
     float max = abs(steps_z);
     float speed = fmin(maxspeed, settings.position.s);
     float speedz = fmin(maxspeed_z, settings.position.s);
-    float n_speed = speed / 60;
-    float n_speed_z = speedz / 60;
+    float speede = fmin(maxspeed_e, settings.position.s);
+
+    float n_speed = speed / 60.0;
+    float n_speed_z = speedz / 60.0;
+    float n_speed_e = speede / 60.0;
+
     float t = abs(dz/n_speed_z); //t - общее время при макс скорости в секундах
     if (abs(steps_a) > max)
     {   max = abs(steps_a);
@@ -29,31 +33,30 @@ void PrinterController::calc_steps_speed(float dx, float dy, float dz, float de,
         t = abs(db/n_speed);}
     if (abs(steps_e) >= max)
     {   max = abs(steps_e);
-        t = abs(de/n_speed);}
+        t = abs(de/n_speed_e);}
 
     //скорость в микрошагах/с
-    float a_speed = (steps_a)/t;
-    float b_speed = (steps_b)/t;
-    float e_speed = (steps_e)/t;
-    float z_speed = (steps_z)/t;
+    float a_speed = (steps_a) / t;
+    float b_speed = (steps_b) / t;
+    float e_speed = (steps_e) / t;
+    float z_speed = (steps_z) / t;
 
     //коэффициент коррекции тактовой частоты по модулю, равный количеству необходимых для движения импульсов
-    speed_a = uint32_t(abs(frequency / a_speed));
-    speed_b = uint32_t(abs(frequency / b_speed));
-    speed_e = uint32_t(abs(frequency / e_speed));
-    speed_z = uint32_t(abs(frequency / z_speed));
+    speed_a = (uint32_t) abs(frequency / a_speed);
+    speed_b = (uint32_t) abs(frequency / b_speed);
+    speed_e = (uint32_t) abs(frequency / e_speed);
+    speed_z = (uint32_t) abs(frequency / z_speed);
 }
 
 void PrinterController::correction(int a_numofmicrosteps, int b_numofmicrosteps, int z_numofmicrosteps,
                                    int e_numofmicrosteps, float &dx, float &dy, float &dz, float &de)
 {
-    float da = a_numofmicrosteps * rotlength / stepsperrot / microsteps;
-    float db = b_numofmicrosteps * rotlength / stepsperrot / microsteps;
+    float da = a_numofmicrosteps / DEFAULT_AXIS_STEPS_PER_UNIT[0];
+    float db = b_numofmicrosteps / DEFAULT_AXIS_STEPS_PER_UNIT[1];
     dx = (da + db) / 2;
     dy = (da - db) / 2;
-    de = e_numofmicrosteps * rotlength / stepsperrot / microsteps;
-    float dl = z_numofmicrosteps * rotlength / stepsperrot / microsteps;
-    dz = dl * h / circlelength;
+    de = e_numofmicrosteps / DEFAULT_AXIS_STEPS_PER_UNIT[3];
+    dz = z_numofmicrosteps / DEFAULT_AXIS_STEPS_PER_UNIT[2];
 }
 
 void PrinterController::gcode_G0(const Parameters& parameters) {
@@ -62,6 +65,7 @@ void PrinterController::gcode_G0(const Parameters& parameters) {
 
 void PrinterController::gcode_G1(const Parameters& parameters) {
     float dx = 0, dy = 0, dz = 0, de = 0;
+
     if (parameters.find('F'))
         settings.position.s = parameters['F'];
     if (settings.position.xyz_type == Absolute) {
@@ -94,25 +98,30 @@ void PrinterController::gcode_G1(const Parameters& parameters) {
 
     // каретка не может уйти в минус по координатам
     if (settings.position.x + dx < 0)
-        dx = 0 - settings.position.x;
+        dx = 0.0 - settings.position.x;
     if (settings.position.y + dy < 0)
-        dy = 0 - settings.position.y;
+        dy = 0.0 - settings.position.y;
     if (settings.position.z + dz < 0)
-        dz = 0 - settings.position.z;
-
-    settings.position.x += dx;
-    settings.position.y += dy;
-    settings.position.z += dz;
-    settings.position.e += de;
-
-    update_parameters();
-    screen.update();
+        dz = 0.0 - settings.position.z;
 
     int32_t steps_a = 0, steps_b = 0, steps_z = 0, steps_e = 0;
     uint32_t speed_a = 0, speed_b = 0, speed_z = 0, speed_e = 0;
-    int32_t ra, rb, rz, re;
+    int32_t ra = 0, rb = 0, rz = 0, re = 0;
 
     calc_steps_speed(dx, dy, dz, de, speed_a, speed_b, speed_z, speed_e, steps_a, steps_b, steps_z, steps_e);
+
+    float ddx = 0, ddy = 0, ddz = 0, dde = 0;
+
+    correction(  steps_a, steps_b, steps_z, steps_e,
+                 ddx, ddy, ddz, dde);
+
+    settings.position.x += ddx;
+    settings.position.y += ddy;
+    settings.position.z += ddz;
+    settings.position.e += dde;
+
+    update_parameters();
+    screen.update();
 
     mechanics.move_extrude( steps_a, steps_b, steps_z, steps_e,
                             speed_a, speed_b, speed_z, speed_e,
